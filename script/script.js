7aa1074c8bbc0ac94e4060ce3e52131ca4f8142c
Sergey-Karpenok <Shamanito@rambler.ru>
2021-04-30 14:29:33 +0300

    Добавил calcSum()

--- script/script.js
+++ script/script.js
@@ -326,7 +326,32 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     phoneCheck();
 
-    // делаем калькуолятор
+    // делаем калькулятор
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue;
+            console.log('typeValue: ', typeValue);
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target === calcType || target === calcSquare || target === calcCount || target === calcDay) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
 
 
 });
